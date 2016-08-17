acbe357bf0c68a2fd3aebe7e1efc690d66bbc90e
Nicholas Bollweg <nbollweg@continuum.io>
2016-08-17 09:51:55 -0400

    fix up meta for proper 2.7 builds

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,3 +1,3 @@
 pip install -r requirements.txt
 cd "${SRC_DIR}"
-py.test --cov=cookiecutter -k "not _hg_ and not mercurial and not test_make_sure_path_exists"
+py.test --cov=cookiecutter -k "not _hg_ and not mercurial and not test_make_sure_path_exists and not pypackage_git"
