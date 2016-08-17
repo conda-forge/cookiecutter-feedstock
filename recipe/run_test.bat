12831746d80544ed65fcba4518db813bf4e7ea82
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-08-17 13:27:35 -0400

    recipe: Fix-up test dependencies and simplify the recipe.

--- recipe/run_test.bat
+++
@@ -1,4 +0,0 @@
-pip install -r requirements.txt
-cd "%SRC_DIR%"
-py.test --cov=cookiecutter -k "not _hg_ and not mercurial and not test_make_sure_path_exists and not pypackage_git"
-if errorlevel 1 exit 1
